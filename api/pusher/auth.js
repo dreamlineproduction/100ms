0f4f030eef9378a84ea71eb44ffb8348086c002c
dreamlineproduction <info@dreamlineproduction.in>
2023-02-17 19:11:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/pusher/auth.js
@@ -0,0 +1,33 @@
+import Pusher from "pusher";
+
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.APP_KEY,
+  secret: process.env.APP_SECRET,
+  cluster: process.env.APP_CLUSTER,
+  useTLS: true,
+});
+
+export default function handler(req, res) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  try {
+    const socketId = req.body.socket_id;
+    const channel = req.body.channel_name;
+    const auth = pusher.authenticate(socketId, channel);
+    res.send(auth);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
